46b304e8a11a071fb24ec4f5b719dd0431c1d178
TARUN S S <tarun.s@s.amity.edu>
2025-05-06 11:45:27 +0530

    test2 branch

--- ases.c
+++ ases.c
@@ -8,4 +8,15 @@ int pro(int a, int b)
 {
 	return a*b;
 }
+int main()
+{
+	int x,y;
+	printf("enter two numbers");
+	scanf("%d %d",&x,&y);
+	sum(a,b);
+	pro(a,b);
+	printf("sum is %d",sum);
+	printf("product is %d",pro);
+        return 0;
 
+}
